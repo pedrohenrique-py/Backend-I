725b3190a7ed78567965a6e2fa70f61eb245c2ad
pedrohenrique-py <pedrohenriqueluzalbuquerq@gmail.com>
2025-04-10 10:10:53 -0400

    first commit

---
+++ exercicio_01.js
@@ -0,0 +1,21 @@
+//Exercicio 01
+let nome = 'Pedro Henrique'
+let idade = 16
+console.log(`Olá, meu nome é ${nome} e eu tenho ${idade} anos.`)
+
+//Exercicio 02
+let string = 'OLÁ, TUDO bem?'
+let minusculo = string.toLowerCase()
+console.log(minusculo)
+let maiusculo = string.toUpperCase()
+console.log(maiusculo)
+
+//Exercicio 03
+let number = 5
+console.log(typeof number)
+
+//Exercicio 04
+let peso = 70
+let altura = 1.75
+let imc = peso / (altura * altura).toFixed(2)
+console.log(imc)
